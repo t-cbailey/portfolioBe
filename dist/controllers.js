52583f4313d9ddaf3b3479f21a991151fb25d1ed
t-cbailey <t.cbaileymusic@gmail.com>
2023-07-25 14:05:59 +0100

    changed seed data

--- dist/controllers.js
+++ dist/controllers.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const { findAllProjects, findProjectById } = require("./models");
+const { findAllProjects, findProjectById, sendEmail } = require("./models");
 const endpoints_json_1 = __importDefault(require("./endpoints.json"));
 exports.getEndpoints = (req, res, next) => {
     res.status(200).send({ endpoints: endpoints_json_1.default });
@@ -22,4 +22,10 @@ exports.getProjectById = (req, res, next) => {
         res.status(200).send({ data });
     })
         .catch(next);
+};
+exports.sendContactFrom = (req, res, next) => {
+    const { body } = req;
+    sendEmail(body).then((data) => {
+        res.status(202).send(data);
+    });
 };
--- dist/models.js
+++ dist/models.js
@@ -1,9 +1,19 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const connection_1 = __importDefault(require("./db/connection"));
+const nodemailer = require("nodemailer");
 exports.findAllProjects = () => {
     return connection_1.default
         .collection("projects")
@@ -33,4 +43,32 @@ exports.findProjectById = (project_id) => {
             msg: `No park found for project name: ${project_id}`,
         });
     });
+};
+exports.sendEmail = (body) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "timbaileydevmailer@gmail.com",
+            pass: "ywyjbdhpctijrcoc",
+        },
+    });
+    console.log(body.name);
+    function main() {
+        return __awaiter(this, void 0, void 0, function* () {
+            // send mail with defined transport object
+            const info = yield transporter.sendMail({
+                from: `${body.name} <${body.email}>`,
+                to: "timbaileydev@gmail.com",
+                subject: `${body.subject}`,
+                text: `${body.messageBody}`, // plain text body
+                // html: "<b>Hello world?</b>", // html body
+            });
+            console.log("Message sent: %s", info.messageId);
+        });
+    }
+    return main()
+        .then(() => {
+        return "message sent";
+    })
+        .catch(console.error);
 };
