2e8b329cb7dd264aa7203ba97141fc7b034b7579
t-cbailey <t.cbaileymusic@gmail.com>
2023-07-26 11:12:28 +0100

    working on emulators

--- db/seed.ts
+++ db/seed.ts
@@ -10,24 +10,27 @@ interface ProjectRequest {
 let projectData: Array<ProjectRequest>;
 
 if (process.env.NODE_ENV !== "production") {
+  console.log("in test");
   projectData = require("./data/test.json");
 } else {
+  console.log("in production");
   projectData = require("./data/prod.json");
 }
 
-const auth = admin.auth();
-
 function deleteCollections(): Promise<FirebaseFirestore.WriteResult[][]> {
   return db.listCollections().then((collections) => {
+    console.log("got collections list");
     const projDeletionPromises = collections.map((collection) =>
       collection.get().then((querySnapshot) => {
         const batch = db.batch();
         querySnapshot.docs.forEach((doc) => {
           batch.delete(doc.ref);
         });
+
         return batch.commit();
       })
     );
+
     return Promise.all(projDeletionPromises);
   });
 }
@@ -37,12 +40,14 @@ function createProjects(): Promise<FirebaseFirestore.WriteResult[]> {
     const pid = `project_${index + 1}`;
     return db.collection("projects").doc(pid).set(project);
   });
+
   return Promise.all(parkCreationPromises);
 }
 
 export const seedDatabase = (): Promise<void> => {
   return deleteCollections()
     .then(() => {
+      console.log("delete successful");
       createProjects();
     })
     .then(() => console.log("Seed successful"))
