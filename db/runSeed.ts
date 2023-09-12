412da00cde0d49c024389fb5d7a6d8326c0b9d4c
t-cbailey <t.cbaileymusic@gmail.com>
2023-09-12 14:28:11 +0100

    added protection to seed file to prevent loss of data

--- db/runSeed.ts
+++ db/runSeed.ts
@@ -1,3 +1,9 @@
 import { seedDatabase } from "./seed";
 
-seedDatabase();
+if (process.env.NODE_ENV !== "production") {
+  seedDatabase();
+} else {
+  console.log("NODE_ENV === production - seed cancelled to avoid loss of data");
+}
+
+// seedDatabase();
